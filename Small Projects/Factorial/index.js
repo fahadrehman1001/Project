4c5080f42c0f56575972aef5113ef732e7039f1c
Fahad Rehman <148562877+fahadrehman1001@users.noreply.github.com>
2024-06-19 04:31:55 +0530

    Add files via upload

---
+++ Small Projects/Factorial/index.js
@@ -0,0 +1,21 @@
+/* Write a factorial to calculate the factorial of the number using reduce and for loops */
+
+
+// Method 1 = using reduce
+let a = 6
+function factorial(number) {
+    let arr = Array.from(Array(number+1).keys())
+    let c = arr.slice(1,).reduce((a,b)=> a*b)
+    return c
+}
+console.log(factorial(a))
+
+// Method 2 = for loop
+function fact(number){
+    let fac = 1;
+    for (let index = 1; index <= number; index++) {
+        fac = fac * index
+    }
+    return fac
+}
+console.log(fact(a))
